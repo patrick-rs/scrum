ac4f5e941daf4844822893ab1f750d04641780c4
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2022-10-16 17:18:25 -0400

    Rename agile to scrum

--- create_tables.sql
+++ create_tables.sql
@@ -4,5 +4,4 @@ create database scrum with owner scrum_admin;
 alter role scrum_admin with login;
 
 create table company (id serial primary key, name text, created timestamp without time zone default now(), last_modified timestamp without time zone default now());
-
 create table sprint(id serial, company_id integer references company, name text, created timestamp without time zone default now(), last_modified timestamp without time zone default now());
