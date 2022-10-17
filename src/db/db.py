9e61d3dc9b92c0c6e12eca4adc487d5cab4bcf66
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2022-10-17 14:04:08 -0400

    Add more checks for db connection variables

--- src/db/db.py
+++ src/db/db.py
@@ -5,15 +5,27 @@ import psycopg2.extras
 
 
 class DB:
-    db_env_var_name = "POSTGRES_SCRUM_ADMIN_PASSWORD"
+    db_env_var_password = "POSTGRES_SCRUM_ADMIN_PASSWORD"
+    db_env_var_host = "POSTGRES_SCRUM_HOST"
+    db_env_var_port = "POSTGRES_SCRUM_PORT"
 
     def __init__(self):
+
         password = "password"
-        if self.db_env_var_name in os.environ:
-            password = os.getenv(self.db_env_var_name)
+        if self.db_env_var_password in os.environ:
+            password = os.getenv(self.db_env_var_password)
+
+        host = "localhost"
+        if self.db_env_var_host in os.environ:
+            password = os.getenv(self.db_env_var_host)
+
+        port = "5432"
+        if self.db_env_var_port in os.environ:
+            password = os.getenv(self.db_env_var_port)
 
         conn = psycopg2.connect(
-            host="localhost",
+            host=host,
+            port=port,
             database="scrum",
             user="scrum_admin",
             password=password,
